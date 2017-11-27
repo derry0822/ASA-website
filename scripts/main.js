86f8bcbe15ceebbade16874a1874131fb9659f7f
Du (Derry) Chen <derryother@gmail.com>
2017-11-26 22:03:14 -0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,52 @@
 var main = function(){
+  // Animations
   var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 1000,
     offset: 0
   });
-
   $('#header h1').show('fade', 2000);
-}
+
+  // Form Validation
+  $("#mainForm").on("submit", function() {
+    var formValid = true;
+    var nameIsValid = $("#name").prop("validity").valid;
+    if(nameIsValid) {
+      $("#nameError").hide();
+    } else {
+      $("#nameError").show();
+      formValid = false;
+    }
+
+    //Email
+    if($("#email").prop("validity").valueMissing) {
+      $("#emailError").show();
+      formValid = false;
+
+    } else {
+      $("#emailError").hide();
+    }
+
+    if($("#email").prop("validity").typeMismatch) {
+      $("#emailErrorFill").show();
+      formValid = false;
+
+    } else {
+      $("#emailErrorFill").hide();
+    }
+
+//message
+    var messageValid = $("#msg").prop("validity").valid;
+    if(messageValid){
+      $("#msgError").hide();
+      formValid = true;
+    }else{
+      $("#msgError").show();
+      formValid = false;
+    }
+    return formValid;
+
+  });
+};
+
 
 $(document).ready(main);
