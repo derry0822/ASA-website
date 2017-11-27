590501d41f2552ee9ba60dd736241a17eae625dc
dianecaro350 <dct78@cornell.edu>
2017-11-26 20:12:56 -0500

    final changes to text for eboard page

--- eboard.php
+++ eboard.php
@@ -19,54 +19,42 @@
         <h3> Jad Rahbany </h3>
         <a href="images/Jad.jpg" target="_blank"><img class="headshot" src="images/Jad.jpg" alt="?"></a>
           <h4>Co-President</h4>
+          <!-- Info Taken from Facebook and LinkedIn. All information is public and links to fb photos were given by client-->
           <p> Jad Rahbany is a Senior computer science major in the College of
             Engineering. He has a business minor and strives to work on products that have an impact in the world.
             Jad is originally from Lebanon and serves as the native Arabic speaker in the language house on west
             campus.
+
+            Feel free to reach out at jr978@cornell.edu to start a conversation.
           </p>
       </div>
       <div class="board">
         <h3> Hannah Rashda </h3>
         <a href="images/hanna.jpg" target="_blank"><img class= "headshot" src="images/hanna.jpg" alt="?"></a>
           <h4>Co-President</h4>
-          <p> Hello! My name is Hannah Rashdan and I am a _______ major in the College of
-            Human Ecology. I chose to join because I wanted to find a
-            club that I could personally belong to on campus. As a person from Syria, I have
-            a harder time finding people who I could relate to on a cultural and personal level.
-            Then I found this club and it has had a great impact on my life.
+          <p> Hannah Rashdan is from Texas and is also a member of Kappa Alpha Theta.In her free time she enjoys traveling.
           </p>
       </div>
       <div class="board">
         <h3> Mazen Ibrahim  </h3>
         <a href="images/mazen.jpg" target="_blank"><img class="headshot" src="images/mazen.jpg" alt="?"></a>
           <h4>Treasurer</h4>
-          <p> Hello! My name is Mazen Ibrahim and I am a Computer Science major in the College of
-            ______________________. I chose to join because I wanted to find a
-            club that I could personally belong to on campus. As a person from Syria, I have
-            a harder time finding people who I could relate to on a cultural and personal level.
-            Then I found this club and it has had a great impact on my life.
+          <p> Mazen Ibrahim is Computer Science major who is originally from Egypt but currently lives in New Jersey.
+
           </p>
       </div>
       <div class="board">
       <h3> Yasmeen Mahayni </h3>
         <a href="images/yasmeen.jpg" target="_blank"><img class="headshot" src="images/yasmeen.jpg" alt="?"></a>
           <h4>Philanthropy</h4>
-          <p> Hello! My name is Yasmeen Mahayni and I am a Hotel Administration major. I
-            chose to join because I wanted to find a club that I could personally
-            belong to on campus. As a person from Syria, I have a harder time finding
-            people who I could relate to on a cultural and personal level.
-            Then I found this club and it has had a great impact on my life.
+          <p> Yasmeen Mahayni is a senior studying hospitality administration and management. She speaks Arabic, French, and English.
           </p>
       </div>
       <div class="board">
       <h3> Sara Tayara  </h3>
         <a href="images/unknown.jpg" target="_blank"><img src="images/unknown.jpg" alt="?"></a>
           <h4>Secretary</h4>
-          <p> Hello! My name is Sara Tayara and I am a Computer Science major in the College of
-            _________. I chose to join because I wanted to find a
-            club that I could personally belong to on campus. As a person from Syria, I have
-            a harder time finding people who I could relate to on a cultural and personal level.
-            Then I found this club and it has had a great impact on my life.
+          <p> Sara Tayara contributes to the Arabic Student Association as a Secretary.She is also a member of MECA.
           </p>
       </div>
     </div>
