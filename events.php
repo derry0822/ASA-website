846e37651c20efc469d4a9385d5f1843437afb3a
gracerief <31828864+gracerief@users.noreply.github.com>
2017-11-28 13:14:32 -0500

    Add files via upload

--- events.php
+++ events.php
@@ -3,8 +3,6 @@
 <head>
   <meta charset="UTF-8">
   <title>Arab Student Association At Cornell</title>
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
   <!-- Auxilliary Stylesheets -->
   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
   <!-- Main Stylesheet -->
@@ -16,9 +14,9 @@
     <div id="events">
       <h2>Events</h2>
       <ul>
-        <li>9/26/17: Social event</li>
-        <li>11/18/17: Weekly meeting</li>
-        <li>11/27/17: Weekly meeting</li>
+        <li><h4>9/26/17: Social Event</h4> We will be meeting at 3:30PM in the Duffield Atrium for our first social event of the semester! Light refreshments will be served.</li>
+        <li><h4>11/18/17: Weekly meeting</h4> We will be meeting to discuss what we've been doing over the semester, as well as what we are planning to do in the coming weeks! More details can be found in the listserv email.</li>
+        <li><h4>11/27/17: Weekly meeting</h4> We will be meeting to discuss what we've been doing over the semester, as well as what we are planning to do in the coming weeks! More details can be found in the listserv email.</li>
       </ul>
     </div>
   </div>
