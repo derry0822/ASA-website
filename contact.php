af571887d150386a4e9c3b96cbcd51d2d67807bf
derry0822 <derryother@gmail.com>
2017-11-28 14:39:42 -0500

    fesfessef

--- contact.php
+++ contact.php
@@ -1,3 +1,52 @@
+<?php
+  $submit = $_REQUEST["submit"];
+  $HIDDEN_ERROR_CLASS = "hiddenError";
+  //Form Validation
+  if(isset($submit)) {
+    $name = strip_tags(trim($_REQUEST["name"]));
+    $email = strip_tags(trim($_REQUEST["email"]));
+    $message = strip_tags(trim($_REQUEST["message"]));
+
+    if(!empty($name)) {
+      $nameValid = true;
+    }
+    else {
+      $nameValid = false;
+    }
+
+    if(!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailValid = true;
+    }
+    else {
+      $emailValid = false;
+    }
+
+    if(!empty($message)) {
+      $messageValid = true;
+    }
+    else {
+      $messageValid = false;
+    }
+
+    $formValid = $nameValid && $emailValid && $messageValid;
+
+    if($formValid) {
+      session_start();
+      $_SESSION["name"] = $name;
+      $_SESSION["email"] = $email;
+      $_SESSION["message"] = $message;
+
+      header("Location: form.php");
+      return;
+    }
+  }
+  // Default Behavior
+  else {
+    $nameValid = true;
+    $emailValid = true;
+    $messageValid = true;
+  }
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,25 +65,30 @@
       <form action="thanks.php" method="post" id="mainForm" novalidate>
         <div class="name">
           <label for="name">Name:</label>
-          <input type="text" id="name" name="name" placeholder="Full Name" required>
-          <span class="errorContainer hiddenError" id="nameError">
+          <input type="text" id="name" name="name" placeholder="Full Name" value="<?php echo($name);?>" required>
+          <span class="errorContainer <?php if ($nameValid) {echo($HIDDEN_ERROR_CLASS);} ?>" id="nameError">
             Name is required.
           </span>
         </div>
         <div class="email">
           <label for="email">E-mail:</label>
-          <input type="email" id="email" name="email" placeholder="user@example.com" required>
-          <span class="errorContainer hiddenError" id="emailError">
+          <input type="email" id="email" name="email" placeholder="user@example.com" value="<?php echo($email);?>" required>
+          <span class="errorContainer <?php if ($emailValid) {echo($HIDDEN_ERROR_CLASS);} ?>" id="emailError">
             Email Address is required.
           </span>
-          <span class="errorContainer hiddenError" id="emailErrorFill">
+          <span class="errorContainer <?php if ($emailValid) {echo($HIDDEN_ERROR_CLASS);} ?>" id="emailErrorFill">
             Not a valid Email Address.
           </span>
         </div>
         <div class="message">
           <label for="msg">Message:</label>
-          <textarea id="msg" name="message" placeholder="Message" required></textarea>
-          <span class="errorContainer hiddenError" id="msgError">
+          <textarea id="msg" name="message" placeholder="Message" required><?php
+            if(isset($_REQUEST["message"]))
+            {
+              echo htmlentities($_REQUEST["message"], ENT_QUOTES);
+            }
+            ?></textarea>
+          <span class="errorContainer <?php if ($messageValid) {echo($HIDDEN_ERROR_CLASS);} ?>" id="msgError">
             Please write a message.
           </span>
         </div>
