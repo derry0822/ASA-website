f3b6570fd23b01108a0624cfdc18caa33fafc8c7
danielsanky <dansanky@yahoo.com>
2017-11-27 23:33:43 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -20,17 +20,17 @@
       <div class="left-info">
         <h2>About us</h2>
         <p> The purpose of Arab Student Association at Cornell University is to create
-            a forum for individuals interested in Arab language, culture, or politics
-            to meet and build community.
+          a forum for individuals interested in Arab language, culture, or politics
+          to meet and build community.
         </p>
         <p>Those individuals will then have the opportunity to increase awareness
-            of pertinent issues in the Arab world both on the Cornell campus and
-            in the larger Ithaca community. The ASA is an apolitical, non-partisan
-            student organization. There will be no association with or bias towards
-            regional Arab political, religious, and ideological entities.
+          of pertinent issues in the Arab world both on the Cornell campus and
+          in the larger Ithaca community. The ASA is an apolitical, non-partisan
+          student organization. There will be no association with or bias towards
+          regional Arab political, religious, and ideological entities.
         </p>
         <p> Our weekly meetings are on Fridays at 3:30pm. Locations subject to change
-            so join our mailing list to find out more!
+          so join our mailing list to find out more!
         </p>
       </div>
       <img src="images/about.jpg" alt="group">
@@ -39,9 +39,27 @@
       <h1> STAY UPDATED. JOIN OUR MAILING LIST! </h1>
       <form id="mail">
         <input type="email" id="mailinglist" placeholder="name@example.com">
+        <div class="button">
+          <button type="submit" name="button">Send</button>
+        </div>
       </form>
     </div>
   </div>
+  <?php
+
+  if(isset($_POST['button'])){
+    $to      = 'dc784@cornell.edu';
+    $subject = 'Hello';
+    $message = 'Welcome to the Cornell ASA email list.';
+    $headers = "From: " . $_POST['button'] . "\r\n" .
+    "CC: ds869@cornell.edu";
+
+    mail($to, $subject, $message,$headers);
+
+    echo 'Email Sent.';
+  }
+
+  ?>
   <!-- Scripts -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
